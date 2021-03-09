94e892a18f30ded92a97ccd3941e54b38958c9be
Yeri <yeri@accelbyte.net>
2021-03-09 18:45:49 +0700

    merge start script

--- run.sh
+++ run.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
+# dind
+docker run --name jenkins-docker --rm --detach \
+  --privileged --network jenkins --network-alias docker \
+  --env DOCKER_TLS_CERTDIR=/certs \
+  --volume jenkins-docker-certs:/certs/client \
+  --volume jenkins-data:/var/jenkins_home \
+  --publish 2376:2376 docker:dind
+
+# jenkins
 docker run --name jenkins --rm --detach \
   --dns 8.8.8.8 \
   --network jenkins --env DOCKER_HOST=tcp://docker:2376 \
